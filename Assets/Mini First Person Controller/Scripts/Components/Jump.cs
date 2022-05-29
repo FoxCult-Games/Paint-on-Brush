d40a9ec2204791219514adf0577eebaf5c85851c
JaworroChanUwU <blazejjaworskixd@gmail.com>
2022-05-29 21:52:29 +0200

    #16 Changing Unity Collab to Github

---
+++ Assets/Mini First Person Controller/Scripts/Components/Jump.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Jump : MonoBehaviour
+{
+    Rigidbody rigidbody;
+    public float jumpStrength = 2;
+    public event System.Action Jumped;
+
+    [SerializeField, Tooltip("Prevents jumping when the transform is in mid-air.")]
+    GroundCheck groundCheck;
+
+
+    void Reset()
+    {
+        // Try to get groundCheck.
+        groundCheck = GetComponentInChildren<GroundCheck>();
+    }
+
+    void Awake()
+    {
+        // Get rigidbody.
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void getJump(InputAction.CallbackContext ctx){
+        if(ctx.performed && (!groundCheck || groundCheck.isGrounded)){
+            rigidbody.AddForce(Vector3.up * 100 * jumpStrength);
+            Jumped?.Invoke();
+        }
+    }
+}
---
+++ Assets/Mini First Person Controller/Scripts/FirstPersonMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class FirstPersonMovement : MonoBehaviour
+{
+    public float speed = 5;
+
+    [Header("Running")]
+    public bool canRun = true;
+    public bool IsRunning { get; private set; }
+    public float runSpeed = 9;
+
+    Rigidbody rigidbody;
+    /// <summary> Functions to override movement speed. Will use the last added override. </summary>
+    public List<System.Func<float>> speedOverrides = new List<System.Func<float>>();
+
+    Vector2 moveAxis = Vector2.zero;
+
+    void Awake()
+    {
+        // Get the rigidbody on this.
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        if(moveAxis == Vector2.zero) return;
+
+        // Get targetMovingSpeed.
+        float targetMovingSpeed = IsRunning ? runSpeed : speed;
+        if (speedOverrides.Count > 0)
+        {
+            targetMovingSpeed = speedOverrides[speedOverrides.Count - 1]();
+        }
+
+        // Get targetVelocity from input.
+        Vector2 targetVelocity = new Vector2( moveAxis.x * targetMovingSpeed, moveAxis.y * targetMovingSpeed);
+
+        // Apply movement.
+        rigidbody.velocity = transform.rotation * new Vector3(targetVelocity.x, rigidbody.velocity.y, targetVelocity.y);
+    }
+
+    public void getMovementAxis(InputAction.CallbackContext ctx){
+        moveAxis = ctx.ReadValue<Vector2>();
+    }
+
+    public void getRunning(InputAction.CallbackContext ctx){
+        IsRunning = ctx.performed && canRun;
+    }
+}
